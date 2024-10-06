f91080e0c980cee1ff85698da6d148a0b6b5ffc1
azishio <azishio3141@gmail.com>
2024-10-06 10:32:08 +0900

    ✨ 基本的な打ち返しサーバーの実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use socketioxide::extract::{Data, SocketRef};
+use socketioxide::SocketIo;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/tiles", |socket: SocketRef| {
+        // TODO Adding authentication
+        socket.on("broadcast_request", |socket: SocketRef, Data(message): Data<String>| {
+            socket.broadcast().emit("update", &message).ok();
+        });
+    });
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Nahlun!" }))
+        .layer(layer);
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
