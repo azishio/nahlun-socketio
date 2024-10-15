38d8b69bb6096b9873d6397b4af1841e6cef8271
azishio <azishio3141@gmail.com>
2024-10-16 01:56:20 +0900

    💬 テスト用のエンドポイントのメッセージとsocketioのパスを変更

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::routing::get;
 use axum::Router;
 use socketioxide::extract::{Data, SocketRef};
 use socketioxide::SocketIo;
+use std::env;
 use tokio::net::TcpListener;
 
 #[cfg(not(target_env = "msvc"))]
@@ -15,17 +16,20 @@ static GLOBAL: Jemalloc = Jemalloc;
 async fn main() {
     let (layer, io) = SocketIo::new_layer();
 
-    io.ns("/tiles", |socket: SocketRef| {
+    io.ns("/water_surface", |socket: SocketRef| {
         // TODO Adding authentication
         socket.on("broadcast_request", |socket: SocketRef, Data(message): Data<String>| {
             socket.broadcast().emit("update", &message).ok();
         });
     });
 
+    let addr = env::var("SOCKETIO_HOST").unwrap();
+
+    let listener = TcpListener::bind(addr.clone()).await.unwrap();
+
     let app = Router::new()
-        .route("/", get(|| async { "Hello, Nahlun!" }))
+        .route("/", get(|| async move { format!("Hello, Nahlun! by {addr}\nI'm SocketIO Container.\n") }))
         .layer(layer);
 
-    let listener = TcpListener::bind("localhost:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
